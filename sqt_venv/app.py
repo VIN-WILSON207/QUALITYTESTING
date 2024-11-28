a08ec4ba57fe23510e502f6723f6f34405c7282b
ABILATEZIE VIN-WILSON ANU <153403524+VIN-WILSON207@users.noreply.github.com>
2024-11-28 18:06:49 +0100

    add error404 to app.py

--- sqt_venv/app.py
+++ sqt_venv/app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify
 app = Flask(__name__) 
 
 
-@app.route('/')
+@app.route('/Test_Path')
 def home():
     return jsonify(message="Hello level 400 FET, Quality Assurance!")
 
