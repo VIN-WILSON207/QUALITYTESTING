e495558ec235a08adc1e07df8be21ae70981904e
ABILATEZIE VIN-WILSON ANU <153403524+VIN-WILSON207@users.noreply.github.com>
2024-11-28 20:43:44 +0100

    add error500 to app.py

--- sqt_venv/app.py
+++ sqt_venv/app.py
@@ -4,8 +4,11 @@ from flask import Flask, jsonify
 app = Flask(__name__) 
 
 
-@app.route('/Test_Path')
+@app.route('/')
 def home():
+     # Intentionally cause a 500 Internal Server Error
+    result = None
+    return result.some_nonexistent_method()  # This will raise an AttributeError
     return jsonify(message="Hello level 400 FET, Quality Assurance!")
 
 
